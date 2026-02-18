e2f3504f366547480096178250adfac4ba6b9bb9
knowledgeimprovementpoint <knowledgeimprovementpoint@gmail.com>
2026-02-18 16:31:48 +0545

    fix:explore offering removed from home page

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,21 +24,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/about": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/about.js"
-    ],
-    "/contact": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/contact.js"
-    ],
-    "/courses": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/courses.js"
-    ],
     "/resources": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26be4f8d27ad3777"; }
+/******/ 		__webpack_require__.h = function() { return "3d3d08850bef004f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
