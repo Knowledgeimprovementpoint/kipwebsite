bdfc1d5b6bc814fc225466545575771c3ff355fc
knowledgeimprovementpoint <knowledgeimprovementpoint@gmail.com>
2026-02-18 16:36:22 +0545

    fix:login register btn removed from headers

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3d3d08850bef004f"; }
+/******/ 		__webpack_require__.h = function() { return "c8217443479712cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
